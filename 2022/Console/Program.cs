1fa39428fb8af2ff784900cd1e813daf1636a8c6
Felix Auer <felixkarlauer@gmail.com>
2022-12-02 13:42:17 +0100

    Load Day Class by newest input

--- 2022/Console/Program.cs
+++ 2022/Console/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Reflection;
 using Console.Days;
 
-var day = new Day02();
+var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+    @"inputs");
+var inputFiles = Directory.GetFiles(path, "*.txt")
+    .Select(Path.GetFileNameWithoutExtension)
+    .ToArray();
+Array.Sort(inputFiles);
+var newest = inputFiles.Reverse().First();
+var className = "Console.Days.Day" + newest;
+var type = Type.GetType(className);
+var day = (IDay)Activator.CreateInstance(type);
 day.Solve();
