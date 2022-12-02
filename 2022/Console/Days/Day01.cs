a1227839a7556bf6cd60f680efe409cfc415ae8f
Felix Auer <felixkarlauer@gmail.com>
2022-12-02 12:47:34 +0100

    Move Day to Class

---
+++ 2022/Console/Days/Day01.cs
@@ -0,0 +1,33 @@
+﻿namespace Console.Days;
+
+using System.Reflection;
+using System;
+
+public class Day01 : IDay
+{
+    public void Solve()
+    {
+        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"inputs\01.txt");
+        var inputs = File.ReadAllLines(path);
+
+        var calories = new List<int>();
+        var current = 0;
+
+        foreach (var weight in inputs)
+        {
+            if (weight.Equals(""))
+            {
+                calories.Add(current);
+                current = 0;
+                continue;
+            }
+
+            current += int.Parse(weight);
+        }
+
+        calories.Sort();
+        calories.Reverse();
+        Console.WriteLine(calories[0]);
+        Console.WriteLine(calories[0] + calories[1] + calories[2]);
+    }
+}
--- 2022/Console/Program.cs
+++ 2022/Console/Program.cs
@@ -1,25 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Reflection;
 
-string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"inputs\01.txt");
-string[] inputs = File.ReadAllLines(path);
+using Console.Days;
 
-List<int> calories = new List<int>();
-int current = 0;
-
-foreach (string weight in inputs)
-{
-    if (weight.Equals(""))
-    {
-        calories.Add(current);
-        current = 0;
-        continue;
-    }
-    current += Int32.Parse(weight);
-    
-}
-calories.Sort();
-calories.Reverse();
-Console.WriteLine(calories[0]);
-Console.WriteLine(calories[0]+calories[1]+calories[2]);
+var day = new Day01();
+day.Solve();
