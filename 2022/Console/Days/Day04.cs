9c026f5b5f22d4ee9a881aea7a86dd8498e284a4
Felix Auer <felixkarlauer@gmail.com>
2022-12-04 11:43:55 +0100

    Day 4

---
+++ 2022/Console/Days/Day04.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Console.Days;
+
+public class Day04 : IDay
+{
+    public void Solve()
+    {
+        var inputs = AoCHelper.ReadLines("04");
+
+
+        PartOne(inputs);
+        PartTwo(inputs);
+    }
+
+    private static void PartOne(string[] inputs)
+    {
+        var fullyContained = 0;
+        var regex = new Regex(@"(\d+)-(\d+),(\d+)-(\d+)");
+        foreach (var line in inputs)
+        {
+            var borders = regex.Match(line).Groups;
+
+            if ((int.Parse(borders[1].Value) <= int.Parse(borders[3].Value) &&
+                 int.Parse(borders[2].Value) >= int.Parse(borders[4].Value)) ||
+                (int.Parse(borders[3].Value) <= int.Parse(borders[1].Value) &&
+                 int.Parse(borders[4].Value) >= int.Parse(borders[2].Value)))
+            {
+                fullyContained++;
+            }
+        }
+
+        System.Console.WriteLine(fullyContained);
+    }
+
+    private static void PartTwo(string[] inputs)
+    {
+        var overlapping = 0;
+        var regex = new Regex(@"(\d+)-(\d+),(\d+)-(\d+)");
+        foreach (var line in inputs)
+        {
+            var borders = regex.Match(line).Groups;
+
+            if ((int.Parse(borders[1].Value) <= int.Parse(borders[3].Value) &&
+                 int.Parse(borders[2].Value) >= int.Parse(borders[3].Value)) ||
+                (int.Parse(borders[3].Value) <= int.Parse(borders[1].Value) &&
+                 int.Parse(borders[4].Value) >= int.Parse(borders[1].Value)))
+            {
+                overlapping++;
+            }
+        }
+
+        System.Console.WriteLine(overlapping);
+    }
+}
