3d2a0bd741035ec921d569eab94cabe3b30eb09e
Felix Auer <felixkarlauer@gmail.com>
2022-12-06 09:41:06 +0100

    Day 6

---
+++ 2022/Console/Days/Day06cs.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Console.Days;
+
+public class Day06 : IDay
+{
+    public void Solve()
+    {
+        var inputs = AoCHelper.ReadLines("06")[0];
+
+
+        CheckSignal(inputs, 4);
+        CheckSignal(inputs, 14);
+    }
+
+    private static void CheckSignal(string inputs, int length)
+    {
+        var dict = new Dictionary<char, int>();
+        for (var c = 'a'; c <= 'z'; c++)
+        {
+            dict.Add(c, 0);
+        }
+
+        var signals = inputs.ToCharArray();
+        for (var i = 0; i < length; i++)
+        {
+            dict[signals[i]]++;
+        }
+
+        for (var right = length; right < signals.Length; right++)
+        {
+            var left = right - length;
+            dict[signals[left]]--;
+            dict[signals[right]]++;
+            if (dict.Count(count => count.Value > 1) == 0)
+            {
+                System.Console.WriteLine(right + 1);
+                break;
+            }
+        }
+    }
+}
