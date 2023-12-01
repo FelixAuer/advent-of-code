6c4b2c95572aeb781fc99cc10df405e1c6d61c03
Felix Auer <felixkarlauer@gmail.com>
2023-12-01 10:39:30 +0100

    Codestyle

--- 2023/AoC2023/Days/Day01.cs
+++ 2023/AoC2023/Days/Day01.cs
@@ -45,19 +45,18 @@ public class Day01 : IDay
             return number;
         }
 
-        switch (number)
+        return number switch
         {
-            case "one": return "1";
-            case "two": return "2";
-            case "three": return "3";
-            case "four": return "4";
-            case "five": return "5";
-            case "six": return "6";
-            case "seven": return "7";
-            case "eight": return "8";
-            case "nine": return "9";
-        }
-
-        return "";
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => ""
+        };
     }
 }
