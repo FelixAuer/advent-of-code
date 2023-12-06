99ece798ab91d4f8d7f1a4b23e9004f59b697539
Felix Auer <felixkarlauer@gmail.com>
2023-12-06 07:11:52 +0100

    Day 6

---
+++ 2023/AoC2023/Days/Day06.cs
@@ -0,0 +1,34 @@
+﻿namespace Console.Days;
+
+public class Day06 : IDay
+{
+    public void Solve()
+    {
+        List<(int time, int maxDistance)> races = new()
+        {
+            (42, 308),
+            (89, 1170),
+            (91, 1291),
+            (89, 1467)
+        };
+        var sum1 = races.Select(CountWins).Aggregate((a, x) => a * x);
+        System.Console.WriteLine(sum1);
+
+        // Part 2: Simple Maths
+    }
+
+    public int CountWins((int time, int maxDistance) race)
+    {
+        var count = 0;
+        for (var i = 0; i < race.time; i++)
+        {
+            var travelTime = race.time - i;
+            if ((travelTime * i) > race.maxDistance)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
